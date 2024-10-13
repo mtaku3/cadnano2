cbefb0b704c92ca78194de7ba7fce5fcb5763410
mtaku3 <m.taku3.1222@gmail.com>
2024-10-13 13:37:22 +0900

    fix: find_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/mtaku3/cadnano2',
     author='mtaku3',
     license='MIT',
-    packages=['cadnano2'],
+    packages=find_packages(),
     package_data={'cadnano': ['ui/mainwindow/images/*.svg', 'ui/mainwindow/images/*.png']},
     install_requires=['PyQt5'],
     entry_points = {'console_scripts': ['cadnano2 = cadnano2.main:main']},
